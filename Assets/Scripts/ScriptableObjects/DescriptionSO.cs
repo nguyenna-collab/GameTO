728fc39ebee597c5ecfe1ffac045f0572d08254b
Bắp <117089665+Nguyenne094@users.noreply.github.com>
2025-08-18 17:52:01 +0700

    Init Project

---
+++ Assets/Scripts/ScriptableObjects/DescriptionSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class DescriptionSO : ScriptableObject {
+    [TextArea]
+    public string Description;
+}
---
+++ Assets/Scripts/System/SceneManagement/LoadSceneRequestSO.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LoadSceneRequestSO", menuName = "Event/LoadSceneRequestSO", order = 0)]
+public class LoadSceneRequestSO : EventBaseSO {
+    
+}
